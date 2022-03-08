bab7c7ee5c0760709573e470e8352688a6c5e964
Igor Loncar <igor023@gmail.com>
2022-03-08 10:09:44 +0100

    inital commit

---
+++ apps/admin/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+const UX_MODULES = [CardModule, ToolbarModule, ButtonModule, TableModule];
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'products',
+                component: DashboardComponent
+            },
+            {
+                path: 'categories',
+                component: CategoriesListComponent
+            },
+            {
+                path: 'orders',
+                component: DashboardComponent
+            },
+            {
+                path: 'users',
+                component: DashboardComponent
+            }
+        ]
+    }
+];
+
+@NgModule({
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }), ...UX_MODULES],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'admin-categories-list',
+    templateUrl: './categories-list.component.html',
+    styleUrls: ['./categories-list.component.scss']
+})
+export class CategoriesListComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
