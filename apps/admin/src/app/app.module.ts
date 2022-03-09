91974a0ec153eb8964d34f5269af2d9044735558
Igor Loncar <igor023@gmail.com>
2022-03-09 16:17:49 +0100

    categories-form component

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,60 +1,74 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+// SERVICES
+import { CategoriesService } from '@nx-repo/products';
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
-
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+// UX
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
-import { CategoriesService } from '@nx-repo/products';
+import { InputTextModule } from 'primeng/inputtext';
 
-const UX_MODULES = [CardModule, ToolbarModule, ButtonModule, TableModule];
+const UX_MODULES = [CardModule, ToolbarModule, ButtonModule, TableModule, InputTextModule];
 
 const routes: Routes = [
-    {
-        path: '',
-        component: ShellComponent,
-        children: [
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'products',
-                component: DashboardComponent
-            },
-            {
-                path: 'categories',
-                component: CategoriesListComponent
-            },
-            {
-                path: 'orders',
-                component: DashboardComponent
-            },
-            {
-                path: 'users',
-                component: DashboardComponent
-            }
-        ]
-    }
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'products',
+        component: DashboardComponent
+      },
+      {
+        path: 'categories',
+        component: CategoriesListComponent
+      },
+      {
+        path: 'categories-form',
+        component: CategoriesFormComponent
+      },
+      {
+        path: 'orders',
+        component: DashboardComponent
+      },
+      {
+        path: 'users',
+        component: DashboardComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
-        ...UX_MODULES
-    ],
-    providers: [CategoriesService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+    CategoriesListComponent,
+    CategoriesFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+    ...UX_MODULES
+  ],
+  providers: [CategoriesService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ apps/admin/src/app/categories/categories-form/categories-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'admin-categories-form',
+    templateUrl: './categories-form.component.html',
+    styleUrls: ['./categories-form.component.scss']
+})
+export class CategoriesFormComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
