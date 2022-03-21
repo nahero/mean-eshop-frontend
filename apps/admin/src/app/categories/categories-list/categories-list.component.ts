2da7f5de250dcc07d3ab6971649b37944be0405f
Igor Loncar <igor023@gmail.com>
2022-03-21 17:55:29 +0100

    table sorting and cleanup, repo-level environments

--- apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { CategoriesService, Category } from '@nx-repo/products';
 import { ToastService } from '@nx-repo/ui';
 import { ConfirmationService } from 'primeng/api';
